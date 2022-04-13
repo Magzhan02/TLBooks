7037bde622aea8fe98969e1d874f1cc9e0c0ab56
Magzhan <sunmorring@gmail.com>
2022-04-13 21:57:23 +0600

    created scss file

--- src/App.js
+++ src/App.js
@@ -2,7 +2,28 @@ import React from "react";
 
 function App() {
   return (
-  <div>123</div>
+      <div className="wrp">
+       <div className="body">
+         <div className="body_menu">
+          <menu>
+            <ul>
+              <li>
+                <img src="/image/book.svg" alt="icon"></img>
+              </li>
+              <li>
+                <img src="/image/user.svg" alt="user"></img>
+              </li>
+              <li>
+                <img src="/image/menu.svg" alt="menu"></img>
+              </li>
+              <li>
+              <img src="/image/sun.svg" alt="menu"></img>
+              </li>
+            </ul>
+          </menu>
+         </div>
+       </div>
+    </div>
   );
 }
 
