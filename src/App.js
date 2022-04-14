0dca9e704c9f6ed3ebe9e2b472e3328d92aa30a4
Magzhan <sunmorring@gmail.com>
2022-04-14 20:20:01 +0600

    create filter block

--- src/App.js
+++ src/App.js
@@ -8,20 +8,38 @@ function App() {
           <menu>
             <ul>
               <li>
-                <img src="/image/book.svg" alt="icon"></img>
+                <img src="/image/book.svg" alt="icon"/>
               </li>
               <li>
-                <img src="/image/user.svg" alt="user"></img>
+                <img src="/image/user.svg" alt="user"/>
               </li>
               <li>
-                <img src="/image/menu.svg" alt="menu"></img>
+                <img src="/image/menu.svg" alt="menu"/>
               </li>
               <li>
-              <img src="/image/sun.svg" alt="menu"></img>
+              <img src="/image/sun.svg" alt="menu"/>
               </li>
             </ul>
           </menu>
          </div>
+
+        <div className="body_right">
+            <div className="header_panel">
+              <span className="speedbar">Книги</span>              
+            </div>
+              <main className="main-left">
+                  <from className="filter-block">
+                    <div className="filter-cont">
+                      <div className="filter-group">
+                        <label for="other-name">Ключевое слово</label>
+                        <input type="text" autocomplete="off" placeholder="Главный герой"/>
+                      </div>
+                      <button className="showcont-btn">Больше параметров</button>
+                     {/*  <div className="filter-more"></div>*/}
+                    </div>
+                  </from>
+              </main>
+        </div>
        </div>
     </div>
   );
