ff9a2078467c7ace821149495ad7af15b72712b0
Magzhan <sunmorring@gmail.com>
2022-04-15 20:38:08 +0600

    complate filter block

--- src/App.js
+++ src/App.js
@@ -35,7 +35,44 @@ function App() {
                         <input type="text" autocomplete="off" placeholder="Главный герой"/>
                       </div>
                       <button className="showcont-btn">Больше параметров</button>
-                     {/*  <div className="filter-more"></div>*/}
+                      <div className="filter-more">
+                        <div className="filter-combo">
+                          <div className="form-group">
+                            <label>Год выпуска от</label>
+                            <input type="text" className="year" autocomplete="off" placeholder="1990"/>
+                          </div>
+                          <div className="form-group">
+                            <label>Год выпуска до</label>
+                            <input type="text" className="year" autocomplete="off" placeholder="2022"/>
+                          </div>
+                        </div>
+                        <div className="filter-combo">
+                          <div className="form-group">
+                            <label>Количество страниц от</label>
+                            <input type="text" className="year" autocomplete="off" placeholder="50"/>
+                          </div>
+                          <div className="form-group">
+                            <label>Количество страниц до</label>
+                            <input type="text" className="year" autocomplete="off" placeholder="1000"/>
+                          </div>
+                        </div>
+                        <div className="filter-combo">
+                          <div className="form-group">
+                            <label>Язык оригинала</label>
+                            <div className="checklist">
+                              <button>Китайский</button>
+                              <button>Корейский</button>
+                              <button>Русский</button>
+                              <button>Японский</button>
+                              <button>Английский</button>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                    <div className="group-btn">
+                      <button className="search">Поиск</button>
+                      <button className="clear">Сбросить</button>
                     </div>
                   </from>
               </main>
